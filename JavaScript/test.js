c46346816ecfef084fc3d4f774641ce3c2f2dedd
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2018-02-05 10:58:03 -1000

    update test alert

---
+++ JavaScript/test.js
@@ -0,0 +1,13 @@
+function popupVisibility(id, buttonText) {
+	let e = document.getElementById(id);
+	if(buttonText != null) {
+		document.getElementById('alertType').textContent = buttonText;
+	}
+	
+	if(e.style.display == 'block') {
+		e.style.display = 'none';
+	} else {
+		e.style.display = 'block';
+	}
+	
+}

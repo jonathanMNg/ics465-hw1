219e3cfdf476f84c5bcab750f0e3289b64dc42d0
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2018-02-07 16:10:40 -1000

    simplify code
    make the codes less redundant and more readable

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,13 +1,1 @@
-function popupVisibility(id, buttonText) {
-  let e = document.getElementById(id);
-  if(buttonText != null) {
-    document.getElementById('alertType').textContent = buttonText;
-  }
-
-  if(e.style.display == 'block') {
-    e.style.display = 'none';
-  } else {
-    e.style.display = 'block';
-  }
-
-}
+//javascript function for index.js only

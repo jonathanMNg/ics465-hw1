5b73fa03f0cc86d5b5a929149c69ff350cb4ea2d
jonathanmng <31453488+jonathanMNg@users.noreply.github.com>
2018-02-10 01:49:25 -1000

    add comment to code

--- JavaScript/shared.js
+++ JavaScript/shared.js
@@ -1,4 +1,10 @@
 //javascript function for all html files
+
+//this function will get the element for the hidden elements
+//then it will display the elements on trigger if the item is hidden
+//otherwise, it will be hidden again.
+//It also get the buttonText from the button, to implement it to
+//the popup, to display the types of threat in the popup message
 function popupVisibility(id, buttonText) {
   let e = document.getElementById(id);
   if(buttonText != null) {
@@ -12,7 +18,13 @@ function popupVisibility(id, buttonText) {
   }
 
 }
-
+//this function is just like popupVisibility
+//however, instead of display type of threat,
+//this function will be trigger if the user click continue
+//to send alert.
+//and prompt the user to type "Serious Threat" (get the "input" element from id)
+//if user entered correct, the warning will be sent.
+//otherwise it will display error
 function typeToConfirm(id) {
   let e = document.getElementById(id);
   if(e.value == "Serious Threat")
@@ -26,7 +38,9 @@ function typeToConfirm(id) {
   }
   e.value = "";
 }
-function init() {
+//this function will add the popupVisibility to every button
+//in the html element when window is onload
+window.onload = function init() {
   let buttons =document.getElementsByClassName('button');
   _.map(buttons, function(button) {
     if(button.getAttribute('onclick') != '') {
@@ -34,4 +48,3 @@ function init() {
     }
   });
 }
-window.onload = init;
